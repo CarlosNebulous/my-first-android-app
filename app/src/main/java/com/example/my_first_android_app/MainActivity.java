2fed3d1e0b2edd5f72c9c2653ef9cb640a6088c6
Álvarez, Carlos <carlos@gumior.es>
2021-03-28 23:40:41 +0200

    first activity layout finished

--- app/src/main/java/com/example/my_first_android_app/MainActivity.java
+++ app/src/main/java/com/example/my_first_android_app/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.my_first_android_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.app_icon);
+
+
+
     }
 }
