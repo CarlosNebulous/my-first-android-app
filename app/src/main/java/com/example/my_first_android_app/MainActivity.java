763b2cd641804e775543b3a0917b05a941610cdc
Álvarez, Carlos <carlos@gumior.es>
2021-04-02 19:14:39 +0200

    code of MainActivity.java finished

--- app/src/main/java/com/example/my_first_android_app/MainActivity.java
+++ app/src/main/java/com/example/my_first_android_app/MainActivity.java
@@ -2,22 +2,44 @@ package com.example.my_first_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.app_good);
 
+        Button btn_next = findViewById(R.id.buttonNext);
+        EditText name = findViewById(R.id.editTextPerson_name);
+        String NAME = name.getText().toString();
 
+    btn_next.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (!NAME.equals("")) {
+                Intent intent = new Intent(MainActivity.this, AgeOptionsActivity.class);
+                intent.putExtra("name", NAME);
+            } else {
+                btn_next.setVisibility(View.GONE);
+                btn_next.setEnabled(false);
+                Toast.makeText(MainActivity.this, R.string.ErrorNameEmpty, Toast.LENGTH_LONG).show();
+                btn_next.setVisibility(View.VISIBLE);
+                btn_next.setEnabled(true);
+            }
+        }
+    });
 
     }
 }
--- app/src/main/java/com/example/my_first_android_app/sharing_activity.java
+++ app/src/main/java/com/example/my_first_android_app/sharing_activity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class sharing_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sharing_activity);
+
+        // Back arrow
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
     }
 }
